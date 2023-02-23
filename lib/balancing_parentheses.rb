bb062e90b4740d86340dcbcd7c39b14a5112f88c
Brandon Castro <castroyou999@gmail.com>
2023-02-23 13:57:35 -0500

    commit tree 856171f85024727daac06ad7323ac6616c6b03a2

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,39 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+    imbalance = 0
+
+    # string.split("").each do |i|
+    #     if stack.peek == "(" && i == ")"
+    #         stack.pop
+    #     else
+    #         stack.push(i)
+    #     end
+    # end
+    # stack.size
+
+    # method for multicharacter strings
+    string.split("").each do |i|
+        if i == "(" || i == ")" 
+            stack.push(i)
+        end
+    end
+
+    until stack.peek == ")" || stack.size == 0
+        stack.pop
+        imbalance += 1
+    end
+
+    while stack.size != 0
+        char = stack.pop
+        if char == ")"
+            imbalance += 1
+        elsif char == "("
+            imbalance -= 1
+        end
+    end
+    imbalance
+    
+end
